ed8f87351c2dab1d8bc09abce28c7bb41ffb550d
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-08-17 14:47:20 -0500

    starter code

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     console.log("Hello world")
+// });
+
+// app.listen(3001, () => {
+//     console.log("server is listening")
+// });
